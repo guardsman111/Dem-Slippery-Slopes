96843979ba63c628404d895a286ca76161af1ca5
Charlie Parsons <33025261+guardsman111@users.noreply.github.com>
2019-12-05 15:58:37 +0000

    Version 1.14.1
    Particle system update - not working at the moment

---
+++ Assets/Scripts/Player Scripts/Particle_Effects_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle_Effects_Script : MonoBehaviour
+{
+    public Animator Rider;
+    public ParticleSystem pSys;
+    private bool racing = false;
+
+    private void FixedUpdate()
+    {
+        if (!racing)
+        {
+            if (Rider.GetBool("Racing"))
+            {
+                pSys.Play(true);
+                racing = true;
+            }
+        }
+        else
+        {
+            pSys.emission.rateOverTime;
+        }
+    }
+}
